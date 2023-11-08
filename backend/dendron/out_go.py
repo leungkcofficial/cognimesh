4843f9b8b89b77b3fb38060361bb8c3310141325
KaChun Leung <leungkc.kachun@gmail.com>
2023-11-08 19:19:02 +0800

    20231108

--- backend/dendron/out_go.py
+++ backend/dendron/out_go.py
@@ -1,7 +1,8 @@
+from backend.axon.in_come import File
+from backend.settings.backend_setting import PGDocStore
+from backend.neuron.neuron import Memory, Cognition
+from langchain.document_loaders import PDFPlumberLoader
 from logger import setup_logger
-from ..axon.in_come import File
-from ..neuron.neuron import Memory
-from ..settings.backend_setting import PGDocStore
 
 
 logger = setup_logger(__name__)
@@ -14,11 +15,20 @@ def process_file(path: str,
     file.import_path(filepath=path,
                      chunk_size=chunk_size,
                      chunk_overlap=chunk_overlap)
-    mem_neuron = Memory()
-    embed = mem_neuron.create_vector(file = file, 
-                                     loader_class=loader_class)
-    doc_store = PGDocStore()
-    doc_store.add_document(file=file)
+    if not file.check_duplicate_in_db():
+    # If not, process the file
+        cognition = Cognition()
+        embed = cognition.create_vector(file=file, loader_class=loader_class)
+
+        store = PGDocStore()
+        memory = Memory(store)
+
+        # Add document metadata and vector embeddings to the database
+        doc_id = memory.add_document(file=file)
+        vector_ids = memory.add_vectors(file=file, embed=embed)
+        memory.update_document_vectors(doc_id, vector_ids)
+    else:
+        print(f"File with SHA1 {file.file_sha1} is already processed.")
     # cur.execute("""
     #     INSERT INTO vector (doc_id, embeddings) VALUES (%s, %s)
     # """, (doc_id, vector.tolist()))
